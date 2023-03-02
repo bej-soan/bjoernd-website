d2c9079d69d68d154812dce6068c2f3295f35a87
BjoernDammann <git@bjoern-d.de>
2023-03-02 17:24:33 +0100

    add fonts and update config

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,19 @@
 const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass);
-
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPassthroughCopy("./src/images/");
+  eleventyConfig.addPassthroughCopy("./src/assets/");
+  eleventyConfig.addWatchTarget("./src/sass/");
+  eleventyConfig.setBrowserSyncConfig({
+		files: './_public/css/**.css'
+	});
   return {
     dir: {
       input: "src",
-      output: "public",
+      output: "_public",
     },
   };
 };

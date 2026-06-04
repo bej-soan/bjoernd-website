3fce782d3f38d0a17ae4b63e4e96cf8b82bba55a
Björn Dammann <git@bjoern-d.de>
2026-06-04 10:08:27 +0200

    update 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,36 @@
-const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+// 1. Moderne ESM-Importe statt 'require'
+import eleventySass from "@11tyrocks/eleventy-plugin-sass-lightningcss";
+import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
+import UpgradeHelper from "@11ty/eleventy-upgrade-help";
 
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(eleventySass);
+// 2. 'export default' statt 'module.exports'
+export default function (eleventyConfig) {
+  // Plugins aktivieren
+  
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(UpgradeHelper);
+  
+  // Passthrough Copies (unverändert)
   eleventyConfig.addPassthroughCopy("./src/images/");
   eleventyConfig.addPassthroughCopy("./src/assets/");
   eleventyConfig.addPassthroughCopy("./src/.htaccess");
   eleventyConfig.addPassthroughCopy("./src/google1e6b9b49852585a8.html");
+  
+  // Watch Targets
   eleventyConfig.addWatchTarget("./src/sass/");
+  
+  eleventyConfig.addPlugin(eleventySass, {
+    // Hier fügen wir die geforderten Compile-Optionen für SCSS hinzu
+    compileOptions: {
+      permalink: "raw"
+    }
+  });
+
+  // BrowserSync Konfiguration
   eleventyConfig.setBrowserSyncConfig({
-		files: './_public/css/**.css'
-	});
+    files: './_public/css/**.css'
+  });
+  
   return {
     dir: {
       input: "src",
